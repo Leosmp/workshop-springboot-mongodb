cdb0670b8b32090167ed4bc281e763b05be4f816
Leosmp <leosoutomaior@.com>
2020-09-24 16:51:39 -0300

    Atualização de usuário com PUT

--- src/main/java/com/leosmp/workshopmongo/services/UserService.java
+++ src/main/java/com/leosmp/workshopmongo/services/UserService.java
@@ -36,8 +36,20 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
-	public User fromDTO(UserDTO objDTO) {
-		return new User(objDTO.getId(), objDTO.getNome(), objDTO.getEmail());
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(obj);
 		
 	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());		
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getNome(), objDTO.getEmail());		
+	}
+
 }
